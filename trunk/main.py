2d3615a09e0513553dcb9263ff19200c4892c29a
pv@swooby.com <pv@swooby.com@9707f62e-3b4d-0410-b0e9-8fca28d0fb7b>
2008-05-22 21:05:54 +0000

    Rearranging code to be a bit cleaner

--- trunk/main.py
+++ trunk/main.py
@@ -17,41 +17,58 @@ API_KEYS = {
   '127.0.0.1':'ABQIAAAA0lf9s5m2g3l_sMAsMUUB8xRi_j0U6kJrkFvY4-OX2XYmEAa76BTDvoB_Rt6S6xEivow6nooxId20Fw',
   }
 DEBUG = True
+  
 
 class MainPage(webapp.RequestHandler):
   
+  def hostname(self):
+    return self.request.host.split(':')[0]
+  
+  def api_key(self):
+    return API_KEYS.get(self.hostname(),'')
+
+  def is_production(self):
+    return self.hostname() == PRODUCTION_HOST
+  
+  def url_admin(self):
+    url_admin = None
+    if users.is_current_user_admin():
+      if self.is_production():
+        url_admin = 'http://appengine.google.com/dashboard?app_id='+APP_NAME
+      else:
+        url_admin = '/_ah/admin/'
+    return url_admin  
+  
+  def url_auth(self):
+    url_auth = None
+    uri = self.request.uri
+    if users.get_current_user():
+      url_auth = users.create_logout_url(uri)
+    else:
+      url_auth = users.create_login_url(uri)
+    return url_auth    
+  
   def get(self):
+
+    request = self.request
+    api_key = self.api_key()
     current_user = users.get_current_user()
+    debug_client = not self.is_production() # TODO(pv): or certain user(s)?
+    url_admin = self.url_admin()
+    url_auth = self.url_auth()
 
     user_list = {}
 #    query = datastore.Query('User')
 #    for user in query.Get(100):
 #      user_list[user['user'].email()] = user['user']
-    
 #    if current_user:
 #      user_list[current_user.email()] = current_user
 
-    request = self.request
-    hostname = request.host.split(':')[0]
-    uri = request.uri
-            
-    url_admin = None
-
-    if current_user:
-      url_auth = users.create_logout_url(uri)
-      if users.is_current_user_admin():
-        if hostname == PRODUCTION_HOST:
-          url_admin = 'http://appengine.google.com/dashboard?app_id='+APP_NAME
-        else:
-          url_admin = '/_ah/admin/'
-    else:
-      url_auth = users.create_login_url(uri)
-
     template_values = {
-      'debug_mode': DEBUG,
-      'api_key': API_KEYS.get(hostname,''),
-      'user_list': user_list.values(),
+      'api_key': api_key,
       'current_user': current_user,
+      'debug_client': debug_client,
+      'user_list': user_list.values(),
       'url_auth': url_auth,
       'url_admin': url_admin,
     }
